3bd10cbaffe93ffe56c85bec8d97f0e464b251d9
Hail91 <aarongillies88@gmail.com>
2020-09-24 15:04:34 -0400

    added comments for basic php data types

--- src/index.php
+++ src/index.php
@@ -1,5 +1,18 @@
 <?php
+// ** Data Types **
+// 1.) Scalar - Single element field
+    // 1.) Boolean (true/false)
+    // 2.) Integer (signed)
+    // 3.) Float/Double (floating pointer number, ex. 1.4)
+    // 4.) String
+// 2.) Compound = Elements which contain multiple parts
+    // 1.) Array ([1, 2, 3, 4], etc.)
+    // 2.) Object 
+    // 3.) Callable (ref to a function)
+    // 4.) iterable (traversable data structure)
+// 3.) Special
+    // 1.) resource (a ref to an external resource)
+    // 2.) Null (variable with no value, ref that points to nothing)
 
-print "Your first line of PHP!"
-
+// ** Standard PHP statements **
 ?>
